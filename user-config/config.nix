9a786af9ede910d7e7b003d197a439a6f05f44a0
Francis St-Amour <m_psyco2@hotmail.com>
2016-09-27 12:48:12 -0400

    Added vim with configurations and asciinema in user's config.

--- user-config/config.nix
+++ user-config/config.nix
@@ -1,17 +1,47 @@
 # Put this file in ~/.nixpkgs/config.nix
 # This can be installed with nix-env -i myPackages
 {
+
+  imports = [ ./local.nix ];
+
   packageOverrides = pkgs_: with pkgs_; {
+
+    myVim = vim_configurable.customize {
+      name = "vim";
+
+      vimrcConfig.vam = {
+        knownPlugins = pkgs.vimPlugins;
+        pluginDictionaries = [
+          {names = [
+              "Syntastic"
+              "youcompleteme"
+              "ctrlp"
+              "sensible"
+              "colors-solarized"
+              "vim-colorschemes"
+              "vim-css-color"
+              "vim-startify"
+           ];}
+        ];
+      };
+    };
+
+
     myPackages = with pkgs; buildEnv {
       name = "myPackages";
-      paths = [ emacs25pre
-      
-            # Spell-checking
-            hunspell
-            hunspellDicts.fr-moderne
-            hunspellDicts.fr-any
-            hunspellDicts.en-gb-ize
-            hunspellDicts.es-cu
+      paths = [
+        myVim
+        emacs25pre
+
+        # Spell-checking
+        hunspell
+        hunspellDicts.fr-moderne
+        hunspellDicts.fr-any
+        hunspellDicts.en-gb-ize
+        hunspellDicts.es-cu
+
+        # asciinema is a tool to record (and share) terminal sessions
+        asciinema
       ];
     };
   };
