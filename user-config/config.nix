579e2be2980a5779ffe03376b9f14a05aa911973
Francis St-Amour <m_psyco2@hotmail.com>
2017-10-24 20:24:14 -0400

    Updated user config.

--- user-config/config.nix
+++ user-config/config.nix
@@ -4,10 +4,24 @@
 
   imports = [ ./local.nix ];
 
+  vim.python = true;
+
   packageOverrides = pkgs_: with pkgs_; {
 
     myVim = vim_configurable.customize {
       name = "vim";
+
+      vimrcConfig.customRC = ''
+set tabstop=2
+set expandtab
+set shiftwidth=2
+
+" Save and run the current file on F5
+" nmap <F5> <ESC>:w \|\| :! ./% <CR>
+
+syntax on
+set bg=dark
+'';
 
       vimrcConfig.vam = {
         knownPlugins = pkgs.vimPlugins;
@@ -21,6 +35,7 @@
               "vim-colorschemes"
               "vim-css-color"
               "vim-startify"
+              "rainbow_parentheses"
            ];}
         ];
       };
@@ -31,14 +46,14 @@
       name = "myPackages";
       paths = [
         myVim
-        emacs25pre
+        emacs25
 
         # Spell-checking
         hunspell
         hunspellDicts.fr-moderne
         hunspellDicts.fr-any
         hunspellDicts.en-gb-ize
-        hunspellDicts.es-cu
+        # hunspellDicts.es-cu
 
         # asciinema is a tool to record (and share) terminal sessions
         asciinema
