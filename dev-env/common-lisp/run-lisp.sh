3e55b63fa17660bb51d6d5b8b24ea31d8e16e9a4
Francis St-Amour <m_psyco2@hotmail.com>
2018-10-19 22:47:35 -0400

    Simpler lisp

--- dev-env/common-lisp/run-lisp.sh
+++ dev-env/common-lisp/run-lisp.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -p sbcl lispPackages.quicklisp -i "sh -e"
+#! nix-shell -p rlwrap lispPackages.quicklisp -i "sh -e -- $@"
 
-# Create a variable containing a lisp expression to correctly setup the
-# cffi:*foreign-library-directories* special variable.
-CFFI_FOREIGN_LIB_DIR=`echo $NIX_LISP_LD_LIBRARY_PATH | sed \
-	-re 's/([^:]*)(:|$)/"\1\/" /g'  \
-	-e "s/^/\(defparameter cffi::*foreign-library-directories* '\(/"  \
-	-e "s/\$/))/" `  
-
-# echo $CFFI_FOREIGN_LIB_DIR
-
-# 1. We must first create the package cffi to be able to set cffi:*foreign-library-directories*.
-# 2. We set cffi:*foreign-library-directories*.
-# 3. We load quicklisp
-# 4. Welcome message (why not)
-# 5. Toplevel and user options (see sbcl --help for distinctions)
-#    Note, that this way we cannot pass other runtime options
-exec sbcl --noinform \
-	--eval '(defpackage :cffi)' \
-	--eval "${CFFI_FOREIGN_LIB_DIR}" \
-	--eval '(load "~/quicklisp/setup.lisp")' \
-	--eval '(format t "Welcome~%")' \
-	"$@"
+rlwrap quicklisp run
 
