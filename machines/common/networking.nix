e6ec0b47924902db283fb1663a26ca2c2d1cd073
Francis St-Amour <m_psyco2@hotmail.com>
2021-12-20 21:02:06 -0500

    update networking.nix

--- machines/common/networking.nix
+++ machines/common/networking.nix
@@ -4,8 +4,6 @@
   # For development
   networking.hosts = {
 	"192.168.0.10" = [ "kthanks.local" ]; # ethernet, static address (no dhcp)
-	# "192.168.0.50" = [ "kthanks.local" ]; # wifi, static address (no dhcp)
-	"192.168.0.105" = [ "gamma.local" ]; # wifi, static dhcp address
+	# "10.0.0.11" = [ "kthanks.local" ]; # wifi, static address (no dhcp)
   };
 }
-
